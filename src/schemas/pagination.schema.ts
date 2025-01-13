6c03e4b2af8f6d2cf6612a0d5db6b6497ee36841
Mathew Goldsborough <mat@nimblebrain.ai>
2025-01-12 15:41:51 -1000

    adding pagination query schema

--- src/schemas/pagination.schema.ts
+++ src/schemas/pagination.schema.ts
@@ -1,5 +1,14 @@
 import { z } from "./zodSetup";
 
+export const PaginationQuerySchema = z
+  .object({
+    page: z.number().default(1),
+    limit: z.number().default(10),
+  })
+  .openapi("Message");
+
+export type PaginationQuery = z.infer<typeof PaginationQuerySchema>;
+
 export const createPaginatedResponseSchema = <T extends z.ZodType>(schema: T, name: string) => {
   return z
     .object({
