dd9893ab7c3a1b720a595eece379b80d164bbfd1
Mathew Goldsborough <mat@nimblebrain.ai>
2025-05-14 16:01:50 -1000

    adding status code

--- src/schemas/responses.schema.ts
+++ src/schemas/responses.schema.ts
@@ -6,6 +6,7 @@ export const HttpStatusCodeEnum = z
   .enum([
     "400", // Bad Request
     "401", // Unauthorized
+    "402", // Payment Required
     "403", // Forbidden
     "404", // Not Found
     "409", // Conflict
